903c938746d9d083d65e1aaf193012672299e2de
nanfeng979 <306338401@qq.com>
2023-10-03 20:22:39 +0800

    补充参数、增加train函数，优化其它函数

--- Assets/Scripts/Qtable.cs
+++ Assets/Scripts/Qtable.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Qtable : MonoBehaviour
 {
-    public int[,] qtable = new int[16, 4];
+    public float[,] qtable = new float[16, 4];
+
+    // params
+    private int n_training_episodes = 10000;
+    private float learning_rate = 0.7f;
+    private int n_eval_episodes = 100;
+    private string env_id = "FrozenLake-v1";
+    private int max_steps = 99;
+    private float gamma = 0.95f;
+    private int[] eval_seed = {};
+    private float max_epsilon = 1.0f;
+    private float min_epsilon = 0.05f;
+    private float decay_rate = 0.0005f;
 
     private void Awake() {
         initTable(qtable);
@@ -22,7 +34,7 @@ public class Qtable : MonoBehaviour
         
     }
 
-    private void initTable(int[,] table) {
+    private void initTable(float[,] table) {
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 4; j++) {
                 table[i, j] = 0;
@@ -30,7 +42,7 @@ public class Qtable : MonoBehaviour
         }
     }
 
-    private void showTable(int[,] table) {
+    private void showTable(float[,] table) {
         string s = "";
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 4; j++) {
@@ -42,17 +54,20 @@ public class Qtable : MonoBehaviour
     }
 
     // state count = raw * col = 4 * 4
-    private int greedy_policy(int[,] table, int state) {
-        int action = table[state, 0];
-        for (int i = 1; i < 4; i++) {
-            if (table[state, i] > action) {
-                action = table[state, i];
+    // action = up, down, left, right = 0, 1, 2, 3
+    private int greedy_policy(float[,] table, int state) {
+        int action = 0;
+        float max = table[state, action];
+        for (int action_index = 1; action_index < 4; action_index++) {
+            if (table[state, action_index] > max) {
+                max = table[state, action_index];
+                action = action_index;
             }
         }
         return action;
     }
 
-    private int epsilon_greedy_policy(int[,] table, int state, float epsilon) {
+    private int epsilon_greedy_policy(float[,] table, int state, float epsilon) {
         float random_num = Random.Range(0f, 1f);
         if (random_num > epsilon) {
             int action = greedy_policy(table, state);
@@ -62,5 +77,47 @@ public class Qtable : MonoBehaviour
             int action = Random.Range(0, 4);
             return action;
         }
+    }
+
+    private float[,] train (int n_training_episodes, float min_epsilon, float max_epsilon, float decay_rate, string env, int max_steps, float[,] qtable) {
+        for (int episode = 0; episode < n_training_episodes; episode++) {
+            var epsilon = min_epsilon + (max_epsilon - min_epsilon) * Mathf.Exp(-decay_rate * episode);
+            int state = 0; // env.reset()
+            bool terminated = false;
+
+            for (int step = 0; step < max_steps; step++) {
+                int action = epsilon_greedy_policy(qtable, state, epsilon);
+
+                // new_state, reward, terminated = env.step(action)
+                var new_state = 0; //
+                var reward = 0; //
+                terminated = false; //
+
+                float [] qtable_state = new float[4];
+                for (int action_index = 0; action_index < 4; action_index++) {
+                    qtable_state[action_index] = qtable[state, action_index];
+                }
+
+                qtable[state, action] = qtable[state, action] + learning_rate * (reward + gamma * QtableStateMaxAction(qtable_state) - qtable[state, action]);
+
+                if (terminated) {
+                    break;
+                }
+
+                state = new_state;
+            }
+        }
+
+        return qtable;
+    }
+
+    private float QtableStateMaxAction(float[] qtable_state) {
+        float max = qtable_state[0];
+        for (int action_index = 1; action_index < 4; action_index++) {
+            if (qtable_state[action_index] > max) {
+                max = qtable_state[action_index];
+            }
+        }
+        return max;
     }
 }
